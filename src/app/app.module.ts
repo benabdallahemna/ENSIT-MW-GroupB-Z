ca37760c1602f3e131400c4d1ae4cadd1c2f65f1
benabdallahemna <114814197+benabdallahemna@users.noreply.github.com>
2023-09-07 21:12:02 +0100

    Revert "footer component"
    This reverts commit 9acacc3151a02533497113607ec5bb3d0c415b77.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './componements/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
